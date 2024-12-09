49230556169b4aa7c7bf433ca7b107c298d3edea
basicplayer01 <103487181+basicplayer01@users.noreply.github.com>
2024-12-09 19:54:41 +0900

    프론트 로그인 기능 구현

--- app/src/public/javascript/home/login.js
+++ app/src/public/javascript/home/login.js
@@ -22,5 +22,15 @@ function login() {
         body: JSON.stringify(req),
     })
     .then((res) => res.json())
-    .then((res) => console.log(res));
+    .then((res) => {
+        if(res.success) {
+            location.href = "/";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error(new Error("로그인 중 에러 발생")); // Error: 로그인 중 에러 발생
+        console.error("로그인 중 에러 발생"); // 로그인 중 에러 발생
+    });
 }
